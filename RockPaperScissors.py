a6eb1aa9f02594cc4cca6fd6d7b2ea929259a266
Saksham Garg <gargsaksham138@gmail.com>
2025-07-22 17:09:41 +0530

    codsoft_taskno4

---
+++ RockPaperScissors.py
@@ -0,0 +1,46 @@
+import random
+def user_choice():
+    while True:
+        Player_choice=input("Choose any one of them (rock, paper or scissors): ").lower()
+        if Player_choice in ["rock","paper","scissors"]:
+            return Player_choice
+        else :
+            print("Invalid Input!\n Please Enter (rock, paper or scissors) ")
+def Computer_choice():        
+    choice=("rock","paper","scissors")
+    computer_cho= ''.join(random.choice(choice))
+    return computer_cho
+def game(Player_choice,computer_cho):
+    if Player_choice==computer_cho:
+        return "tie"
+    elif (Player_choice=='rock' and computer_cho=='scissors') or (Player_choice=='scissors'and computer_cho=='paper')or(Player_choice=='paper' and computer_cho=='rock'):
+        return "Player"
+    else:
+        return "Computer"
+player_score=0
+computer_score=0
+Rounds=1
+print("Welcome in Game World!")
+while True:
+    print("\nRound",Rounds)
+    Player_choices=user_choice()
+    computer_choices=Computer_choice()
+    winner=game(Player_choices,computer_choices)
+    print("Player choice =",Player_choices,"\nComputer choice =",computer_choices,"\nWinner =",winner)
+    if winner=="tie":
+        player_score += 0
+        computer_score += 0
+    elif winner=="Player":
+        player_score+=1
+
+    elif winner=="Computer":
+        computer_score+=1
+    print("Player Score =",player_score,"\tComputer Score =",computer_score)
+    
+    player_again=input("Do you want to play again(YES/NO): ").lower()
+    if player_again=="no":
+        print("Thanks for Playing!")
+        break
+    elif player_again=="yes":
+        Rounds+=1
+ 
